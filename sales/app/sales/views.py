4b490bf3127d3854b1368681112a8c0bcf89067f
Python41 <dsgs@>
2024-11-28 20:13:15 +0500

    start

--- sales/app/sales/views.py
+++ sales/app/sales/views.py
@@ -1,19 +1,2 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import loader
-
-
 def index(request):
-    template = loader.get_template("sales/index.html")
-    context = get_context('Главная страница')
-    return HttpResponse(template.render(context, request))
-
-
-def get_context(title, d=None):
-    context = {'title': title,
-               'pages': [('football/', 'Футбол'),
-                         ]}
-    if d:
-        for k in d:
-            context[k] = d[k]
-    return context
+    return 'Hello world'
