dfccd80f3299516e89e2c816f4a5258de66e6ea0
PSVolk <159309202+PSVolk@users.noreply.github.com>
2024-11-28 20:19:30 +0500

    Update models.py

---
+++ sales/app/sales/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    phone = models.CharField(max_length=30)
+    email = models.EmailField
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    info = models.TextField()
+
+
+class Order(models.Model):
+    date = models.DateTimeField()
+    total = models.IntegerField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+class Positions(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    order = models.ForeignKey(Or
