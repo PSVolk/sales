69f7048d4574698687bbacdba348eccaf027dca4
PSVolk <159309202+PSVolk@users.noreply.github.com>
2024-11-28 19:51:41 +0500

    Update views.py

--- sales/app/sales/views.py
+++ sales/app/sales/views.py
@@ -1,2 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+
 def index(request):
-    return 'Hello world'
+    template = loader.get_template("sales/index.html")
+    context = get_context('Главная страница')
+    return HttpResponse(template.render(context, request))
+
+
+def get_context(title, d=None):
+    context = {'title': title,
+               'pages': [('football/', 'Футбол'),
+                         ]}
+    if d:
+        for k in d:
+            context[k] = d[k]
+    return context
